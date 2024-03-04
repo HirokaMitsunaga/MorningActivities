18cf03588e8a7e8a6ea1383820ef1ec5eff94aa6
hiroka <mitsunagahrk87@gmail.com>
2024-03-05 08:15:49 +0900

    add cors & csrf

--- router/router.go
+++ router/router.go
@@ -13,15 +13,15 @@ import (
 func NewRouter(uc controller.IUserController, tc controller.ITaskController) *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localshot:3000", os.Getenv("FE_URL")},
+		AllowOrigins: []string{"http://localhost:3000", os.Getenv("FE_URL")},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept,
 			echo.HeaderAccessControlAllowHeaders, echo.HeaderXCSRFToken},
-		AllowMethods: []string{"GET", "PUT", "`POST", "DELETE"},
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
 		AllowCredentials: true,
 	}))
 	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
-		CookiePath: "/",
-		CookieDomain: os.Getenv("API_DOMAIN"),
+		CookiePath:     "/",
+		CookieDomain:   os.Getenv("API_DOMAIN"),
 		CookieHTTPOnly: true,
 		// CookieSameSite: http.SameSiteNoneMode,
 		CookieSameSite: http.SameSiteDefaultMode,
