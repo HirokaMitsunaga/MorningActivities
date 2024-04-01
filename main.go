0b211631595bb497f757bc1c79904047acc02fdd
hiroka <mitsunagahrk87@gmail.com>
2024-04-02 07:48:50 +0900

    add timeline

--- main.go
+++ main.go
@@ -9,17 +9,20 @@ import (
 	"go-api/validator"
 )
 
-func main(){
+func main() {
 	db := db.NewDB()
 	userValidator := validator.NewuserValidator()
 	taskValidator := validator.NewTaskValidator()
 	userRepository := repository.NewUserRepository(db)
 	taskRepository := repository.NewTaskRepository(db)
+	timelineRepository := repository.NewTimelineRepository(db)
 	userUsecase := usecase.NewUserUsecase(userRepository, userValidator)
 	taskUsecase := usecase.NewTaskUsecase(taskRepository, taskValidator)
+	timelineUsecase := usecase.NewTimelineUsecase(timelineRepository)
 	userController := controller.NewUserController(userUsecase)
 	taskController := controller.NewTaskController(taskUsecase)
-	e := router.NewRouter(userController,taskController)
+	timelineController := controller.NewTimelineController(timelineUsecase)
+	e := router.NewRouter(userController, taskController, timelineController)
 	//エラーが出たらLoggerがエラー出力して、強制終了する
 	e.Logger.Fatal(e.Start(":8080"))
 }
---
+++ usecase/timeline_usecase.go
@@ -0,0 +1,105 @@
+package usecase
+
+import (
+	"go-api/model"
+	"go-api/repository"
+)
+
+type ITimelineUsecase interface {
+	GetAllTimelines() ([]model.TimelineResponse, error)
+	GetTimelineById(timelineId uint) (model.TimelineResponse, error)
+	CreateTimeline(timeline model.Timeline) (model.TimelineResponse, error)
+	UpdateTimeline(timeline model.Timeline, timelineId uint) (model.TimelineResponse, error)
+	DeleteTimeline(timelineId uint) error
+}
+
+type timelineUsecase struct {
+	tlr repository.ITimelineRepository
+}
+
+func NewTimelineUsecase(tlr repository.ITimelineRepository) ITimelineUsecase {
+	return &timelineUsecase{tlr}
+}
+
+func (tlu *timelineUsecase) GetAllTimelines() ([]model.TimelineResponse, error) {
+	timelines := []model.Timeline{}
+	if err := tlu.tlr.GetAllTimelines(&timelines); err != nil {
+		return nil, err
+	}
+	resTimelines := []model.TimelineResponse{}
+	for _, v := range timelines {
+		t := model.TimelineResponse{
+			ID:           v.ID,
+			Sentence:     v.Sentence,
+			LikeCount:    v.LikeCount,
+			CommentCount: v.CommentCount,
+			CreatedAt:    v.CreatedAt,
+			UpdatedAt:    v.UpdatedAt,
+			UserId:       v.UserId,
+		}
+		resTimelines = append(resTimelines, t)
+	}
+	return resTimelines, nil
+}
+
+func (tlu *timelineUsecase) GetTimelineById(timelineId uint) (model.TimelineResponse, error) {
+	timeline := model.Timeline{}
+	if err := tlu.tlr.GetTimelineById(&timeline, timelineId); err != nil {
+		return model.TimelineResponse{}, err
+	}
+	resTask := model.TimelineResponse{
+		ID:           timeline.ID,
+		Sentence:     timeline.Sentence,
+		LikeCount:    timeline.LikeCount,
+		CommentCount: timeline.CommentCount,
+		CreatedAt:    timeline.CreatedAt,
+		UpdatedAt:    timeline.UpdatedAt,
+		UserId:       timeline.UserId,
+	}
+	return resTask, nil
+}
+
+func (tlu *timelineUsecase) CreateTimeline(timeline model.Timeline) (model.TimelineResponse, error) {
+	// if err := tlu.tlv.TaskValidate(task); err != nil {
+	// 	return model.TaskResponse{}, err
+	// }
+	if err := tlu.tlr.CreateTimeline(&timeline); err != nil {
+		return model.TimelineResponse{}, err
+	}
+	resTask := model.TimelineResponse{
+		ID:           timeline.ID,
+		Sentence:     timeline.Sentence,
+		LikeCount:    timeline.LikeCount,
+		CommentCount: timeline.CommentCount,
+		CreatedAt:    timeline.CreatedAt,
+		UpdatedAt:    timeline.UpdatedAt,
+		UserId:       timeline.UserId,
+	}
+	return resTask, nil
+}
+
+func (tlu *timelineUsecase) UpdateTimeline(timeline model.Timeline, timelineId uint) (model.TimelineResponse, error) {
+	// if err := tlu.tv.TaskValidate(task); err != nil {
+	// 	return model.TaskResponse{}, err
+	// }
+	if err := tlu.tlr.UpdateTimeline(&timeline, timelineId); err != nil {
+		return model.TimelineResponse{}, err
+	}
+	resTask := model.TimelineResponse{
+		ID:           timeline.ID,
+		Sentence:     timeline.Sentence,
+		CreatedAt:    timeline.CreatedAt,
+		UpdatedAt:    timeline.UpdatedAt,
+		LikeCount:    timeline.LikeCount,
+		CommentCount: timeline.CommentCount,
+		UserId:       timeline.UserId,
+	}
+	return resTask, nil
+}
+
+func (tlu *timelineUsecase) DeleteTimeline(timelineId uint) error {
+	if err := tlu.tlr.DeleteTimeline(timelineId); err != nil {
+		return err
+	}
+	return nil
+}
