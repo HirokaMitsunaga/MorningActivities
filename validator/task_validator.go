e86e3e933f47d57eff4297259b57230c9ee90e1b
hiroka <mitsunagahrk87@gmail.com>
2024-05-14 09:13:00 +0900

    feature/add-task-validation

--- validator/task_validator.go
+++ validator/task_validator.go
@@ -23,5 +23,16 @@ func (tv *taskValidator) TaskValidate(task model.Task) error {
 			validation.Required.Error("title is required"),
 			validation.RuneLength(1, 10).Error("limited max 10 char"),
 		),
+		validation.Field(
+			&task.ScheduledMinutes,
+			validation.Required.Error("scheduled minutes are required"),
+			validation.Min(1).Error("scheduled minutes must be at least 1"),
+			validation.Max(1440).Error("scheduled minutes must be less than 1440"), // 1日の分数
+		),
+		validation.Field(
+			&task.ActualMinutes,
+			validation.Min(0).Error("actual minutes must be at least 0"),
+			validation.Max(1440).Error("actual minutes must be less than 1440"), // 1日の分数
+		),
 	)
 }
