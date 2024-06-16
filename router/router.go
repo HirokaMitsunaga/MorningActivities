ce4ea6eb8135822aaf3bb8190f70e5bd9aae29b0
hiroka <mitsunagahrk87@gmail.com>
2024-06-17 08:22:41 +0900

    feature/20240613-chnage-https

--- router/router.go
+++ router/router.go
@@ -24,15 +24,15 @@ func NewRouter(uc controller.IUserController, tc controller.ITaskController, tlc
 		CookiePath:     "/",
 		CookieDomain:   os.Getenv("API_DOMAIN"),
 		CookieHTTPOnly: true,
-		CookieSameSite: http.SameSiteLaxMode,
-		//CookieSameSite: http.SameSiteDefaultMode,
-		//CookieMaxAge:   60,
+		// CookieSameSite: http.SameSiteLaxMode,
+		CookieSameSite: http.SameSiteDefaultMode,
+		CookieMaxAge:   86400,
 	}))
-	e.POST("/signup", uc.SignUp)
-	e.POST("/login", uc.LogIn)
-	e.POST("/logout", uc.LogOut)
-	e.GET("/csrf", uc.CsrfToken)
-	t := e.Group("/tasks")
+	e.POST("/api/signup", uc.SignUp)
+	e.POST("/api/login", uc.LogIn)
+	e.POST("/api/logout", uc.LogOut)
+	e.GET("/api/csrf", uc.CsrfToken)
+	t := e.Group("/api/tasks")
 	t.Use(echojwt.WithConfig(echojwt.Config{
 		SigningKey:  []byte(os.Getenv("SECRET")),
 		TokenLookup: "cookie:token",
@@ -43,7 +43,7 @@ func NewRouter(uc controller.IUserController, tc controller.ITaskController, tlc
 	t.PUT("/:taskId", tc.UpdateTask)
 	t.DELETE("/:taskId", tc.DeleteTask)
 
-	tl := e.Group("/timelines")
+	tl := e.Group("/api/timelines")
 	tl.Use(echojwt.WithConfig(echojwt.Config{
 		SigningKey:  []byte(os.Getenv("SECRET")),
 		TokenLookup: "cookie:token",
@@ -55,7 +55,7 @@ func NewRouter(uc controller.IUserController, tc controller.ITaskController, tlc
 	tl.DELETE("/:timelineID", tlc.DeleteTimeline)
 
 	//likeの設定
-	l := e.Group("/likes")
+	l := e.Group("/api/likes")
 	l.Use(echojwt.WithConfig(echojwt.Config{
 		SigningKey:  []byte(os.Getenv("SECRET")),
 		TokenLookup: "cookie:token",
@@ -64,7 +64,7 @@ func NewRouter(uc controller.IUserController, tc controller.ITaskController, tlc
 	l.DELETE("/:likeID", lc.DeleteLike)
 	l.POST("/toggle", lc.ToggleLike)
 
-	com := e.Group("/comments")
+	com := e.Group("/api/comments")
 	com.Use(echojwt.WithConfig(echojwt.Config{
 		SigningKey:  []byte(os.Getenv("SECRET")),
 		TokenLookup: "cookie:token",
